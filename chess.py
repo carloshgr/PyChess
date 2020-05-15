44c696c7868a48b6ea22d76ed54cdc910c0a5aeb
Carlos Henrique <carlos.ribeiro@ccc.ufcg.edu.br>
2020-05-15 19:17:02 -0300

    improvements on pieces placement

--- chess.py
+++ chess.py
@@ -19,19 +19,29 @@ class Board():
     def __init__(self):
         self.WHITE_PAWN = rescale(pygame.image.load("white-pawn.png"))
         self.WHITE_ROOK = rescale(pygame.image.load("white-rook.png"))
-        self.WHITE_BISHOP = pygame.image.load("white-bishop.png")
-        self.WHITE_KNIGHT = pygame.image.load("white-knight.png")
-        self.WHITE_QUEEN = pygame.image.load("white-queen.png")
-        self.WHITE_KING = pygame.image.load("white-king.png")
-        self.BLACK_PAWN = pygame.image.load("black-pawn.png")
-        self.BLACK_ROOK = pygame.image.load("black-rook.png")
-        self.BLACK_BISHOP = pygame.image.load("black-bishop.png")
-        self.BLACK_KNIGHT = pygame.image.load("black-knight.png")
-        self.BLACK_QUEEN = pygame.image.load("black-queen.png")
-        self.BLACK_KING = pygame.image.load("black-king.png")
+        self.WHITE_BISHOP = rescale(pygame.image.load("white-bishop.png"))
+        self.WHITE_KNIGHT = rescale(pygame.image.load("white-knight.png"))
+        self.WHITE_QUEEN = rescale(pygame.image.load("white-queen.png"))
+        self.WHITE_KING = rescale(pygame.image.load("white-king.png"))
+        self.BLACK_PAWN = rescale(pygame.image.load("black-pawn.png"))
+        self.BLACK_ROOK = rescale(pygame.image.load("black-rook.png"))
+        self.BLACK_BISHOP = rescale(pygame.image.load("black-bishop.png"))
+        self.BLACK_KNIGHT = rescale(pygame.image.load("black-knight.png"))
+        self.BLACK_QUEEN = rescale(pygame.image.load("black-queen.png"))
+        self.BLACK_KING = rescale(pygame.image.load("black-king.png"))
 
         self.whitePawnRect = self.WHITE_PAWN.get_rect()
         self.whiteRookRect = self.WHITE_ROOK.get_rect()
+        self.whiteBishopRect = self.WHITE_BISHOP.get_rect()
+        self.whiteKnightRect = self.WHITE_KNIGHT.get_rect()
+        self.whiteQueenRect = self.WHITE_QUEEN.get_rect()
+        self.whiteKingRect = self.WHITE_KING.get_rect()
+        self.blackPawnRect = self.BLACK_PAWN.get_rect()
+        self.blackRookRect = self.BLACK_ROOK.get_rect()
+        self.blackBishopRect = self.BLACK_BISHOP.get_rect()
+        self.blackKnightRect = self.BLACK_KNIGHT.get_rect()
+        self.blackQueenRect = self.BLACK_QUEEN.get_rect()
+        self.blackKingRect = self.BLACK_KING.get_rect()
         
         self.BOARD = [
             ["BR", "BN", "BB", "BQ", "BK", "BB", "BN", "BR"],
@@ -45,19 +55,32 @@ class Board():
         ]
 
 
-    def draw(self):
-        self.draw_board()
+    def draw_board(self):
+        self.draw_grid()
+
+        aux = {
+            "WP" : (self.WHITE_PAWN, self.whitePawnRect),
+            "WR" : (self.WHITE_ROOK, self.whiteRookRect),
+            "WB" : (self.WHITE_BISHOP, self.whiteBishopRect),
+            "WN" : (self.WHITE_KNIGHT, self.whiteKnightRect),
+            "WQ" : (self.WHITE_QUEEN, self.whiteQueenRect),
+            "WK" : (self.WHITE_KING, self.whiteKingRect),
+            "BP" : (self.BLACK_PAWN, self.blackPawnRect),
+            "BR" : (self.BLACK_ROOK, self.blackRookRect),
+            "BB" : (self.BLACK_BISHOP, self.blackBishopRect),
+            "BN" : (self.BLACK_KNIGHT, self.blackKnightRect),
+            "BQ" : (self.BLACK_QUEEN, self.blackQueenRect),
+            "BK" : (self.WHITE_KING, self.blackKingRect)
+        }
 
         for i in range(8):
             for j in range(8):
-                if self.BOARD[i][j] == "WP":
-                    self.whitePawnRect.center = ((j*80)+40, (i*80)+40)
-                    SCREEN_SURF.blit(self.WHITE_PAWN, self.whitePawnRect)
-                if self.BOARD[i][j] == "WR":
-                    self.whiteRookRect.center = ((j*80)+40, (i*80)+40)
-                    SCREEN_SURF.blit(self.WHITE_ROOK, self.whiteRookRect)
+                if self.BOARD[i][j] != -1:
+                    aux[self.BOARD[i][j]][1].center = ((j*80)+40, (i*80)+40)
+                    SCREEN_SURF.blit(aux[self.BOARD[i][j]][0], aux[self.BOARD[i][j]][1])
 
-    def draw_board(self):
+
+    def draw_grid(self):
         color = BLACK
         for sqr_y in range(0, 640, 80):
             if color == WHITE:
@@ -81,7 +104,7 @@ class Chess():
         FPS_CLOCK = pygame.time.Clock()
 
         BOARD = Board()
-        BOARD.draw()
+        BOARD.draw_board()
         pygame.display.update()
 
         while True:
